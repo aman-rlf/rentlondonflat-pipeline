526f38ad056a4eb6479ee75aa061cd6434dc8bc3
aman-rlf <aman.gupta@rentlondonflat.com>
2024-10-26 08:19:40 +0000

    Updated pipelines

--- sql_database_address_log.py
+++ sql_database_address_log.py
@@ -1,4 +1,3 @@
-# flake8: noqa
 from typing import Any
 import os
 
@@ -18,7 +17,7 @@ os.environ["SCHEMA__NAMING"] = "direct"
 
 def load_entire_database() -> None:
     """Use the sql_database source to completely load all tables in a database"""
-    pipeline = dlt.pipeline(pipeline_name="rlf_pipeline", 
+    pipeline = dlt.pipeline(pipeline_name="rlf_pipeline_address_log", 
                             destination='bigquery', 
                             dataset_name="rlf_data_tables", 
                             progress="log",
@@ -27,12 +26,17 @@ def load_entire_database() -> None:
 
     # By default the sql_database source reflects all tables in the schema
     # The database credentials are sourced from the `.dlt/secrets.toml` configuration
-    source = sql_database(backend="pyarrow", chunk_size=100000).parallelize().with_resources(*resource_list_1)
+    source = sql_database(backend="pyarrow", chunk_size=100000).parallelize().with_resources("Address_Log")
 
-    
+    source.Address_Log.apply_hints(
+    incremental=dlt.sources.incremental("Log_Updated_At", initial_value=pendulum.DateTime(2022, 1, 1, 0, 0, 0)),
+    primary_key="LogID"
+)
+
     # Run the pipeline. For a large db this may take a while
-    info = pipeline.run(source, write_disposition="replace")
+    info = pipeline.run(source, write_disposition="merge")
     print(info)
 
 if __name__ == "__main__":
     load_entire_database()
+
